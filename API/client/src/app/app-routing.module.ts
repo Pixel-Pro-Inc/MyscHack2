d030449aedc978875de658d00c908743d63ef6aa
Yewo Theu <yewotheu123456789@gmail.com>
2021-11-03 04:56:17 +0200

    ,njknjknk

--- API/client/src/app/app-routing.module.ts
+++ API/client/src/app/app-routing.module.ts
@@ -5,11 +5,8 @@ import { InternshipComponent } from './internship/internship.component';
 import { LearnmoreComponent } from './learnmore/learnmore.component';
 import { LoginComponent } from './login/login.component';
 import { NatserviceComponent } from './natservice/natservice.component';
-<<<<<<< HEAD
-import { SignupComponent } from './signup/signup.component';
-=======
 import { ProfileComponent } from './profile/profile.component';
->>>>>>> 3db3fee3457a7d8c9a5945b74945ec24682a7971
+import { SignupComponent } from './signup/signup.component';
 import { VolunteerComponent } from './volunteer/volunteer.component';
 
 const routes: Routes = [
@@ -18,12 +15,6 @@ const routes: Routes = [
   {path: 'national_service', component: NatserviceComponent},
   {path: 'volunteer', component: VolunteerComponent},
   {path: 'internship', component: InternshipComponent},
-<<<<<<< HEAD
-  {path: 'signup', component: SignupComponent},
-  {path: 'learnmore', component: LearnmoreComponent}
-=======
-  {path: 'profile', component: ProfileComponent},
->>>>>>> 3db3fee3457a7d8c9a5945b74945ec24682a7971
 ];
 
 @NgModule({
